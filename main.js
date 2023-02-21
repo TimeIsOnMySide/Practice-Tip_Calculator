5639b16cb945628ec16740a1c58ebd940293d371
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-02-20 20:51:09 -0800

    Done!

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // When the text changes, calculate result
 document.querySelector('#bill').addEventListener('change', (findresult));
 document.querySelector('#tip').addEventListener('change', (findresult));
+document.querySelector('#people').addEventListener('change', (findresult));
+
 
 // Initialize variables
 let tipTotal;
@@ -18,8 +20,12 @@ document.querySelector('#tipTotal').textContent = USDollar.format(0);
 document.querySelector('#billTotal').textContent = USDollar.format(0);
 document.querySelector('#tip').value = '15%';
 document.querySelector('#bill').value = '0.00';
+document.querySelector('#people').value = '1';
+document.querySelector('.perPerson1').style.visibility = "hidden";
+document.querySelector('.perPerson2').style.visibility = "hidden";
 
-// limit characters in the text field
+
+// limit characters in the text fields
 const regex = /[0-9.]/;
 document.querySelector("#tip").addEventListener("keypress", event => {
   if (!regex.test(event.key)) {
@@ -30,18 +36,38 @@ document.querySelector("#bill").addEventListener("keypress", event => {
     if (!regex.test(event.key)) {
       event.preventDefault();
     }
-  });
+});
+const peopleRegex = /[1-9]/;
+document.querySelector("#people").addEventListener("keypress", event => {
+    if (!peopleRegex.test(event.key)) {
+      event.preventDefault();
+    }
+});
 
 // Function that gives the totals
 function findresult() {
     var bill = parseInt(document.getElementById('bill').value);
     var tip = parseInt(document.getElementById('tip').value);
-    if(tip) {
+    var people = parseInt(document.getElementById('people').value);
+
+    if(tip) { // Add % to tip text
         document.querySelector('#tip').value = tip.toString() + '%';
     }
-    if(bill && tip) {
+    if(people > 1) { // show the per person text if theres multiple people
+        document.querySelector('.perPerson1').style.visibility = "visible";
+        document.querySelector('.perPerson2').style.visibility = "visible";
+    } else { // hide the per person text if theres one person
+        document.querySelector('.perPerson1').style.visibility = "hidden";
+        document.querySelector('.perPerson2').style.visibility = "hidden";
+    }
+    if(bill && tip && people === 1) { // If theres only one person
         tipTotal = (tip / 100) * bill;
         billTotal = tipTotal + bill;
+        document.querySelector('#tipTotal').textContent = USDollar.format(tipTotal.toString());
+        document.querySelector('#billTotal').textContent = USDollar.format(billTotal.toString());
+    } else if(bill && tip && people > 1) { // If theres more than one person
+        tipTotal = ((tip / 100) * bill) / people;
+        billTotal = (tipTotal + bill) / people;
         document.querySelector('#tipTotal').textContent = USDollar.format(tipTotal.toString());
         document.querySelector('#billTotal').textContent = USDollar.format(billTotal.toString());
     }
